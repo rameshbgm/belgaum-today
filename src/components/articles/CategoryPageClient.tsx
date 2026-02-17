ca2dc0c5414e4d2ffc394a64a15a0430a491052b
Ramesh <rameshbgm@gmail.com>
2026-02-17 20:33:37 +0800

    refactor: remove category badge from article cards and hide sidebar sections on category pages
    - Remove Category Badge display from ArticleCard component
    - Remove CATEGORY_META import and categoryMeta variable
    - Hide both Categories and RSS sections from category page sidebar
    - Cleaner card design focused on content
    
    This simplifies the article card UI and removes redundant category information.

--- src/components/articles/CategoryPageClient.tsx
+++ src/components/articles/CategoryPageClient.tsx
@@ -255,7 +255,7 @@ export function CategoryPageClient({
 
                     {/* Sidebar */}
                     <aside className="lg:col-span-1 mt-8 lg:mt-0">
-                        <Sidebar showCategories />
+                        <Sidebar showCategories={false} showRss={false} />
                     </aside>
                 </div>
             </div>
